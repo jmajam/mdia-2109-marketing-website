08a99abebc402308f6d4ebfe6bf135f0b8b84dc6
jmajam <jmajam2001@gmail.com>
2026-04-12 21:39:43 -0700

    code: add js logic for carousel

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(document.querySelectorAll(".carousel__slide"));
+const prevBtn = document.querySelector(".carousel__nav--prev");
+const nextBtn = document.querySelector(".carousel__nav--next");
 
+let index = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
