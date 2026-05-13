b63e531c9e1956b6b75c41649e3c5943a5d4ada1
rehaan mirani <you@example.com>
2026-05-13 14:56:07 -0700

    Replaced old files with final project files

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ document.querySelectorAll(".btn").forEach((btn) => {
 		console.log("Button clicked:", btn.textContent);
 	});
 });
+
+document.querySelectorAll(".btn:not(.disabled-btn)").forEach((btn) => {
+	btn.addEventListener("click", () => {
+		console.log("Clicked:", btn.textContent);
+	});
+});
